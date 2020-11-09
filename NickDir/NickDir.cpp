947fa142332d8336d38d990c2026dd293703d445
jnarnold007 <jackson.n.arnold@gmail.com>
2020-11-09 14:09:37 -0500

    Day 2 of work on permanent aliasing

--- NickDir/NickDir.cpp
+++ NickDir/NickDir.cpp
@@ -6,6 +6,36 @@
 
 using namespace std;
 
+bool fileExists(const std::string& filename)
+{
+    WIN32_FIND_DATAA fd = { 0 };
+    HANDLE hFound = FindFirstFileA(filename.c_str(), &fd);
+    bool retval = hFound != INVALID_HANDLE_VALUE;
+    FindClose(hFound);
+
+    return retval;
+}
+
+//Test to set alias commands permanently
+int permanent(wchar_t name[33], wchar_t command[132]) {
+    bool exists = fileExists("C:\\Users\\%USERPROFILE%\\alias.cmd");
+
+    std::wofstream outfile("C:\\Users\\%USERPROFILE%\\alias.cmd");
+
+    if (!exists) {
+        cout << "First setup";
+        outfile << L"@echo off" << std::endl;
+    }
+
+    wchar_t prefix[162] = L"DOSKEY";
+
+    wcscat_s(prefix, name);
+
+    outfile << wcscat_s(prefix, command) << std::endl;
+
+    return NULL;
+}
+
 int main()
 {
     wchar_t name[33];
@@ -19,11 +49,13 @@ int main()
     wcin >> tempLocation;
 
     wchar_t location[132] = L"cd ";
-
+    
     wcscat_s(location, tempLocation);
-
+    
+    permanent(name, location);
+    
     LPWSTR wideName = name; //Convert directly from a wchar_t to a LPWSTR (which I now know stands for Long Pointer to Wide String)
-
+    
     LPWSTR wideLocation = location;
 
     wchar_t executable[8] = L"cmd.exe";
@@ -33,33 +65,6 @@ int main()
     AddConsoleAlias(wideName, wideLocation, wideExecutable);
 
     cout << "\nShortcut successfully added. Type the name you entered and you will now automatically navigate to that directory.\n";
-}
-
-//Test to set alias commands permanently
-int Permanent(wchar_t name[33], wchar_t command[132]) {
-    bool exists = FileExists("alias.cmd");
-
-    std::wofstream outfile("alias.cmd");
-
-    if (!exists) {
-        outfile << L"@echo off" << std::endl;
-    }
-
-    wchar_t prefix[162] = L"DOSKEY";
-
-    wcscat_s(prefix, name);
-
-    outfile << wcscat_s(prefix, command) << std::endl;
-}
-
-bool FileExists(const std::string& filename)
-{
-    WIN32_FIND_DATAA fd = { 0 };
-    HANDLE hFound = FindFirstFileA(filename.c_str(), &fd);
-    bool retval = hFound != INVALID_HANDLE_VALUE;
-    FindClose(hFound);
-
-    return retval;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
